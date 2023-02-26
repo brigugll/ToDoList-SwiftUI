56dcdd3feb7e19ecdf9cff9a08eb9ba7648110cc
brigugll <brigugll@bc.edu>
2023-02-26 16:46:46 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Luke Briguglio on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
